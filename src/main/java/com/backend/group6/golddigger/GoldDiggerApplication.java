8c6b66b35d98a5e477bcdaf81f3aacd963cff4d7
jan-beszterda <jan.beszterda@gmail.com>
2022-03-22 13:04:04 +0100

    Initial commit - starting project repo

---
+++ src/main/java/com/backend/group6/golddigger/GoldDiggerApplication.java
@@ -0,0 +1,13 @@
+package com.backend.group6.golddigger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoldDiggerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoldDiggerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/backend/group6/golddigger/GoldDiggerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.group6.golddigger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoldDiggerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
