dcf4d8988a0232e4f09201e9f0079222b5a8a4fc
Jan Beszterda <jan.beszterda@gmail.com>
2022-03-25 14:34:49 +0100

    created ShopInventoryDAO class

---
+++ src/main/java/com/backend/group6/golddigger/dao/ShopInventoryDAO.java
@@ -0,0 +1,21 @@
+package com.backend.group6.golddigger.dao;
+
+import com.backend.group6.golddigger.model.FoodItem;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@Repository
+public class ShopInventoryDAO {
+    private static List<InventoryItem> data = new ArrayList<>(List.of(
+            new InventoryItem(new FoodItem(UUID.randomUUID(), "Apple", 0.2, 0.3), 20.0, 100),
+            new InventoryItem(new FoodItem(UUID.randomUUID(), "Bread", 0.4, 0.5), 30.0, 20),
+            new InventoryItem(new Pickaxe(UUID.randomUUID(), "Wooden pickaxe", 1.0, 100.0), 50.0, 10)
+    ));
+
+    public List<InventoryItem> getInventory() {
+        return data;
+    }
+}
