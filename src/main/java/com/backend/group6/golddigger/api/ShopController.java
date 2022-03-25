6f15c4f67768b28e0f7a88ada1cdac53f8849b82
Jan Beszterda <jan.beszterda@gmail.com>
2022-03-25 13:29:31 +0100

    created shop controller class

---
+++ src/main/java/com/backend/group6/golddigger/api/ShopController.java
@@ -0,0 +1,22 @@
+package com.backend.group6.golddigger.api;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/shop")
+public class ShopController {
+    ShopService shopService;
+
+    public ShopController(ShopService shopService) {
+        this.shopService = shopService;
+    }
+
+    @GetMapping()
+    public List<InventoryItem> getInventory() {
+        return shopService.getInventory();
+    }
+}
