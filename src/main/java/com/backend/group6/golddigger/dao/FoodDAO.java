c7cab0cb47468720cc7480504d1d9905dbaef7b0
Damir1975 <91055438+Damir1975@users.noreply.github.com>
2022-03-28 10:35:00 +0200

    Food Controller, Service and DAO done.

--- src/main/java/com/backend/group6/golddigger/dao/FoodDAO.java
+++ src/main/java/com/backend/group6/golddigger/dao/FoodDAO.java
@@ -1,8 +1,30 @@
 package com.backend.group6.golddigger.dao;
 
+import com.backend.group6.golddigger.model.FoodItem;
 import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @Repository
 public class FoodDAO {
+    public List<FoodItem> DB = new ArrayList<>(List.of(
+            new FoodItem(UUID.randomUUID(), "Bread", 0.2, 0.2),
+            new FoodItem(UUID.randomUUID(), "Meat", 0.5, 0.3),
+            new FoodItem(UUID.randomUUID(), "Water", 0.1, 0.1),
+            new FoodItem(UUID.randomUUID(), "Vegetables", 0.3, 0.2),
+            new FoodItem(UUID.randomUUID(), "Wine", 0.4, 0.6)));
+
+    public List<FoodItem> getAllFoodItems() {
+        return DB;
+    }
+
+    public Optional<FoodItem> getFoodItemById(UUID id) {
+        return getAllFoodItems().stream()
+                .filter(pickaxe -> pickaxe.getId().equals(id))
+                .findFirst();
+    }
 
 }
