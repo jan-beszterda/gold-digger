46876a09751a2ca23a8530bb874597b9393d91fb
Jan Beszterda <jan.beszterda@gmail.com>
2022-03-29 15:34:09 +0200

    added methods to shop classes

---
+++ src/main/java/com/backend/group6/golddigger/repository/ShopRepository.java
@@ -0,0 +1,7 @@
+package com.backend.group6.golddigger.repository;
+
+import com.backend.group6.golddigger.model.Shop;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ShopRepository extends CrudRepository<Shop, Integer> {
+}
