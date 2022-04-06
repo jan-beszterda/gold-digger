2fb83fc078ab28143ff90c1b5a90444145cc4423
Damir1975 <91055438+Damir1975@users.noreply.github.com>
2022-04-06 09:46:44 +0200

    dig() and eat() in playerController

--- src/main/java/com/backend/group6/golddigger/api/PlayerController.java
+++ src/main/java/com/backend/group6/golddigger/api/PlayerController.java
@@ -3,6 +3,7 @@ package com.backend.group6.golddigger.api;
 import com.backend.group6.golddigger.model.Item;
 import com.backend.group6.golddigger.model.Player;
 import com.backend.group6.golddigger.service.*;
+import org.hibernate.query.criteria.internal.ValueHandlerFactory;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -51,9 +52,20 @@ public class PlayerController {
     }
 
     @PostMapping("/{playerId}/buyItem/{itemId}")
-    public void buyItem(@PathVariable("playerId") Integer playerId,  @PathVariable("itemId") Integer itemId) {
+    public void buyItem(@PathVariable("playerId") Integer playerId, @PathVariable("itemId") Integer itemId) {
         Item item = shopService.sellItem(itemId);
         itemService.addItem(item);
         playerService.buyItem(playerId, item);
     }
+
+    @GetMapping("/{playerId}/dig")
+    public void dig(@PathVariable("playerId") Integer playerId) {
+        playerService.dig(playerId);
+    }
+
+    @GetMapping("/{playerId}/eat/{itemId}")
+    public void eat(@PathVariable("playerId") Integer playerId, @PathVariable("itemId") Integer itemId) {
+        playerService.eat(playerId, itemId);
+    }
+
 }
