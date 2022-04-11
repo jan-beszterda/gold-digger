8a7e6144de32b55d6011553fc7694e256457004c
Damir1975 <91055438+Damir1975@users.noreply.github.com>
2022-04-11 15:05:43 +0200

    getMineById()

--- src/test/java/com/backend/group6/golddigger/service/MineServiceTest.java
+++ src/test/java/com/backend/group6/golddigger/service/MineServiceTest.java
@@ -2,13 +2,13 @@ package com.backend.group6.golddigger.service;
 
 import com.backend.group6.golddigger.dao.MineDAO;
 import com.backend.group6.golddigger.model.Mine;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -59,7 +59,29 @@ class MineServiceTest extends MockitoExtension {
 
         //Verify
         assertEquals(false, containingMines);
+    }
 
+    @Test
+    @DisplayName("Verify that getMineById() returns specific mine from DB")
+    void getMineById() {
+        //Setup
+        Mine mine1 = new Mine();
+        mine1.setMineId(1);
+        mine1.setMineName("Mine1");
+
+        Mine mine2 = new Mine();
+        mine2.setMineId(2);
+        mine2.setMineName("Mine2");
+
+        List<Mine> minesFromDB = List.of(mine1, mine2);
+        Mockito.when(mineDAO.getAllMines()).thenReturn(minesFromDB);
+
+        //Test
+        List<Mine> actualListOfMines = unitUnderTest.getAllMines();
+
+        //Verify
+        Assertions.assertAll(() -> assertEquals("Mine1", actualListOfMines.get(0).getMineName()),
+                () -> assertEquals("Mine2", actualListOfMines.get(1).getMineName()));
     }
 
     @Test
@@ -69,4 +91,6 @@ class MineServiceTest extends MockitoExtension {
     @Test
     void addMine() {
     }
+
+
 }
