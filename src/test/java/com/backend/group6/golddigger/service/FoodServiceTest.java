b0c59b5fc248c3af99d42d4dd43432b833b7b8ef
Damir1975 <91055438+Damir1975@users.noreply.github.com>
2022-04-11 12:04:57 +0200

    getAllFoodItems(), getFoodItemById()

--- src/test/java/com/backend/group6/golddigger/service/FoodServiceTest.java
+++ src/test/java/com/backend/group6/golddigger/service/FoodServiceTest.java
@@ -2,7 +2,7 @@ package com.backend.group6.golddigger.service;
 
 import com.backend.group6.golddigger.dao.FoodDAO;
 import com.backend.group6.golddigger.model.FoodItem;
-import com.backend.group6.golddigger.model.Player;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -49,7 +49,43 @@ class FoodServiceTest extends MockitoExtension {
     }
 
     @Test
+    @DisplayName("Verify that getAllFoodItems() returns all foodItems from DB")
+    void getAllFoodItemsShouldReturnFalseIfEmpty() {
+        //Setup
+        List<FoodItem> foodFromDB = List.of();
+        Mockito.when(foodDAO.getAllFoodItems()).thenReturn(foodFromDB);
+
+        //Test
+        List<FoodItem> actualListOfFoodItems = unitUnderTest.getAllFoodItems();
+        boolean containingFoodItems = (actualListOfFoodItems.isEmpty()) ? false : true;
+
+        //Verify
+        assertEquals(false, containingFoodItems);
+
+    }
+
+    @Test
+    @DisplayName("Verify that getFoodItemById() returns specific foodItem from DB")
     void getFoodItemById() {
+        //Setup
+        FoodItem foodItem1 = new FoodItem();
+        foodItem1.setItemId(1);
+        foodItem1.setItemName("Coca Cola");
+
+        FoodItem foodItem2 = new FoodItem();
+        foodItem2.setItemId(2);
+        foodItem2.setItemName("Pepsi");
+
+        List<FoodItem> foodFromDB = List.of(foodItem1, foodItem2);
+        Mockito.when(foodDAO.getAllFoodItems()).thenReturn(foodFromDB);
+
+        //Test
+        List<FoodItem> actualListOfFoodItems = unitUnderTest.getAllFoodItems();
+
+        //Verify
+        Assertions.assertAll(() -> assertEquals("Coca Cola", actualListOfFoodItems.get(0).getItemName()),
+                () -> assertEquals("Pepsi", actualListOfFoodItems.get(1).getItemName()));
+
     }
 
     @Test
